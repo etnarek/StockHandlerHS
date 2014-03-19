60f75ac9d4d607ec6ec8df791e1069f411a96ef8
Bruno Rocha Pereira <brochape@ulb.ac.be>
2014-03-19 18:43:58 +0100

    Changed name to barcode

--- produit/models.py
+++ produit/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)
+    barcode = models.CharField(max_length=200)
     price = models.IntegerField()
