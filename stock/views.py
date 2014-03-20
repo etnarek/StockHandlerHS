b639e281fd1bffe6a23b61c4271278cfff19a848
Romain Fontaine <romain-fontaine@outlook.com>
2014-03-20 17:56:51 +0100

    Added views for the stock

--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from stock.models import Stock
+from django.template import RequestContext, loader
+from produit.models import Product
 
 # Create your views here.
+
+def index(request):
+    stocks = Stock.objects.all()
+    template = loader.get_template('index.html')
+    context = RequestContext(request, {
+        'stocks':stocks,
+    })
+    return HttpResponse(template.render(context))
